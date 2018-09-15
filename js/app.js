67671c3b6033c4f344b5e23fd865d1192b16f118
scorpidio11 <scorpido@gmail.com>
2018-09-14 20:29:01 -0700

    working file

--- js/app.js
+++ js/app.js
@@ -1,116 +1,101 @@
 
+ // Initialize Firebase
+ var config = {
+  apiKey: "AIzaSyA8HNW2WePCw7eGAIL7Av9iOW1AK2OhYIo",
+  authDomain: "train-1ea5c.firebaseapp.com",
+  databaseURL: "https://train-1ea5c.firebaseio.com",
+  projectId: "train-1ea5c",
+  storageBucket: "train-1ea5c.appspot.com",
+  messagingSenderId: "194604634464"
+};
+firebase.initializeApp(config);
+
+     // Create a variable to reference the database
+     var database = firebase.database();
 
 
 
-// SETUP VARIABLES
+// 2. Button for adding Train Schedule
+$("#submitbtn").on("click", function(event) {
+event.preventDefault();
 
+// Grabs user input
+var trainName = $("#train_name").val().trim();
+var destination = $("#destination").val().trim();
+var firstTrain = $("#first_train").val().trim();
+var frequency = $("#frequency").val().trim();
+frequency = parseInt(frequency);
 
-
-
-
-//1. Retrieve user inuts and conver to variables
-
-//2. 3use those variables to run on Ajax call 
-//3. Break down the NYT object into usable fields
-//4. Dynamically generate html contents
-
-
-
-// search parameter
-var authKey    = "2b2863fbe8174574aea2929d0e2be4a9";
-var queryTerm  = "";
-var numResults = 0;
-var startYear  = 0;
-var endYear = 0;
-
-var queryURLBase = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + authKey;
-
-var articleCounter = 0;
-
-// FUNCTIONS When you click search button
-//========================================
-
-
-function runQuery(numArticles, queryURL) {
-
-  $.ajax({
-    url: queryURL,
-    method: 'GET',
-  }).done(function (NYTData) {
-    console.log(queryURL);
-    console.log(NYTData);
-  })
-
-
-
-}
-
-// MAIN PROCESSES
-//========================================
-$('#searchBtn').on('click', function(){
-
-  var queryTerm = $('#search').val().trim();
-  console.log(queryTerm);
-
-  var newURL = queryURLBase + "&q" + queryTerm;
-
-  console.log(newURL);
-
-
-// Get the year
-startYear = $('#startYear').val().trim() + "0101";
-endYear = $('#endYear').val().trim() + "0101";
-
-if (parseInt(startYear)) {
-
-  newURL = newURL + "&begin_date" + startYear;
-
+// Creates local "temporary" object for holding user input data
+var newTrain = {
+  trainName: trainName,
+  destination: destination,
+  firstTrain: firstTrain,
+  frequency: frequency
 };
 
-if (parseInt(endYear)) {
+// Uploads employee data to the database
+database.ref().push(newTrain);
 
-  newURL = newURL + "&end_date" + endYear;
+// Logs everything to console
+console.log(newTrain.trainName);
+console.log(newTrain.destination);
+console.log(newTrain.firstTrain);
+console.log(newTrain.frequency);
 
-};
+//alert("Train successfully added");
 
+// Clears all of the text-boxes
+$("#train_name").val("");
+$("#desitination").val("");
+$("#first_train").val("");
+$("#frequency").val("");
+});
 
-
-
-
-
-runQuery(10,newURL);
-
-return false;
-
-
-})
-
-
-// This button clears the top articles section
-$('#clearAll').on('click', function(){
-	articleCounter = 0;
-	$("#wellSection").empty();
-})
+// 3. Create Firebase event for adding train to the database and a row in the html when a user adds an entry
+database.ref().on("child_added", function(childSnapshot) {
+console.log(childSnapshot.val());
 
 
-
-  // var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-  // url += '?' + $.param({
-  //   'api-key': "2b2863fbe8174574aea2929d0e2be4a9",
-  //   'q': "headline article body",
-  //   'begin_date': "19900101",
-  //   'end_date': "20181231",
-  //   'sort': "newest",
-  //   'hl': "yes",
-  //   'page': 5
-  // });
   
-
-  // var article = result;
-
-
-
-
-
+// Store everything into a variable.
+var trainName = childSnapshot.val().trainName;
+var destination = childSnapshot.val().destination;
+var firstTrain = childSnapshot.val().firstTrain;
+var frequency = childSnapshot.val().frequency;
 
 
+var firstTimeConverted = moment(firstTrain, "HH:mm");
+console.log(firstTimeConverted);
+
+var currentTime = moment().format("HH:mm");
+console.log("CURRENT TIME: " + currentTime);
+
+// store difference between currentTime and fisrt train converted in a variable.
+var timeDiff = moment().diff(moment(firstTimeConverted), "minutes");
+
+console.log("Difference in Time: " + timeDiff);
+// find Remainder of the time left and store in a variable
+var timeRemainder = timeDiff % frequency;
+console.log("Time Till Train: " + timeRemainder);
+// to calculate minutes till train,we store it in a variable
+var minToTrain = frequency - timeRemainder;
+console.log("Minutes to Train: " + minToTrain);
+// next train
+var nxTrain = moment().add(minToTrain, "minutes").format("HH:mm");
+
+
+// Create the new row
+var newRow = $("<tr>").append(
+  $("<td>").text(trainName),
+  $("<td>").text(destination),
+  $("<td>").text(frequency),
+  $("<td>").text(nxTrain),
+  $("<td>").text(minToTrain),
+
+);
+
+// Append the new row to the table
+$(".table > tbody").append(newRow);
+
+});
